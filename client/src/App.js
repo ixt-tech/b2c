9c163c4df3b4544bae3733baf9bfc9b75452e3ba
Ingemar Svensson <ingemar.svensson@ixledger.com>
2018-11-27 19:25:40 +0000

    account update

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
 import Loadable from 'react-loadable';
-import IxtProtect from "./contracts/IxtProtect.json";
-import getWeb3 from "./utils/getWeb3";
-import truffleContract from "truffle-contract";
 import Header from './components/header/index'
 
 import "./App.css";
@@ -21,37 +18,8 @@ const AccountPage = Loadable({
 });
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
-
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const Contract = truffleContract(IxtProtect);
-      Contract.setProvider(web3.currentProvider);
-      const instance = await Contract.deployed();
-console.log(accounts[0]);
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance });
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`
-      );
-      console.log(error);
-    }
-  };
 
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
     return (
       <div className="App">
         <Header />

e466bf396d9834eb881fe04eb475a640a00305d9
Harry Wright <hcawright@gmail.com>
2018-11-28 14:29:24 +0000

    Current progress with adding EIP712 tests.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var IxtProtect = artifacts.require("./IxtProtect.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(IxtProtect);
+module.exports = function(deployer, network, accounts) {
+  deployer.deploy(IxtProtect, accounts[0]);
 };
--- test/IxtProtectTest.js
+++ test/IxtProtectTest.js
@@ -1,37 +1,71 @@
 const IxtProtect = artifacts.require("./IxtProtect.sol");
 
 contract("IXTProtect", async (accounts) => {
+  let ixtProtect;
+
   const deployer = accounts[0];
-  const user_a = accounts[1];
+  const verifier = accounts[1];
+  const user = accounts[2];
+  const Products = { 
+    PERSONAL_PROTECTION: 0
+  };
+
+  const memberTypes = {
+    membershipNumber: "uint256",
+    memberAddress: "address",
+    productsCovered: "uint8[]",
+    invitationCode: "uint256"
+  };
+
+  function memberData() {
+    return {
+      membershipNumber: 123123123,
+      memberAddress: user,
+      productsCovered: [ Products.PERSONAL_PROTECTION ],
+      invitationCode: 0
+    };
+  }
+
+  beforeEach(async => {
+    ixtProtect = await IxtProtect.new(verifier);
+  });
 
   it("Should inherit the Ownable contract", async () => {
-    const instance = await IxtProtect.deployed();
-    const owner =  await instance.owner();
-    assert.equal(owner, accounts[0]);
+    const owner =  await ixtProtect.owner();
+    assert.equal(owner, deployer);
   });
 
   describe("Join function", () => {
 
     it("Should allow a member to join if they have a proof signed by the contract owner", async () => {
-      const instance = await IxtProtect.deployed();
-      // The owner of the contract should be accounts[0]
-      const owner =  await instance.owner();
+      const member = memberData();
 
-      const member_data = {}
-      member_data.membership_number = 123123123;
-      member_data.member_address = accounts[1];
-      member_data.products_covered = 42;
-      member_data.invitation_code = 123123123123;
-
-      // 1. Sign the data with accounts[0]
-      const signature = signMembershipData(member_data, owner);
+      // 1. Sign the data with `verifier` address
+      const ixtAddress = ixtProtect.address;
+      const signature = signMembershipData(member, verifier, ixtAddress);
       // 2. Include the data and digital signature in the payload to join() - this should complete without throwing
       // 3. Assert that the member is now present in the smart contracts storage data
     });
   }); 
 
-  function signMembershipData(membership_data, signing_address) {
+  function signMembershipData(memdata, signer, contractAddress) {
+    // This call equivalent to getMemberHash in contract
+    const memHash = getMemberHash(memdata);
 
+
+    function getMemberHash(member) {
+      return hashEIP712Message(hashMember(member));
+    }
+
+    function hashMember(member) {
+      // abi.encodePacked() the struct
+    }
+
+    function hashEIP712Message() {
+
+    }
   }
+
+
 
 });

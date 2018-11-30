cf74327820f399ac5dfab56f2392aa0a3e862e3b
Harry Wright <hcawright@gmail.com>
2018-11-30 22:19:30 +0000

    Extending ERC20 to allow for minting during testing.

---
+++ contracts/mocks/MockToken.sol
@@ -0,0 +1,33 @@
+pragma solidity 0.5.0;
+
+import "../lib/ERC20.sol";
+
+/**
+ * @title A mock token used for test purposes
+ * which exposes functions that allow minting and
+ * burning of tokens
+*/
+
+contract MockToken is ERC20 {
+
+  function mint(
+    address account,
+    uint256 amount
+  ) public {
+    _mint(account, amount);
+  }
+
+  function burn(
+    address account,
+    uint256 amount
+  ) public {
+    _burn(account, amount);
+  }
+
+  function burnFrom(
+    address account,
+    uint256 amount
+  ) public {
+    _burnFrom(account, amount);
+  }
+}
